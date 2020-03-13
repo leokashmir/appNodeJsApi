bc8c0512d260eff463873bdda14f13257d9ab51b
Leonardo Gonçalves Ferreira Barros <leonardo.barros@deal.br>
2020-03-12 21:55:28 -0300

    Teste para verificar erro no deploy - 01

--- src/routes/index-routes.js
+++ src/routes/index-routes.js
@@ -13,17 +13,6 @@ router.get("/", (req, res, next) => {
 });
 
 
-router.post('/cadastro', [
-    check('email').isEmail(),
-    check('senha').isLength({ min: 6 }).withMessage("senha precisa ter no minimo 6 caracteres.")
-  ], apiskyController.createUser );
-  
-
-  router.post('/auth', apiskyController.postAuth);
-router.get('/listarUsuarios', middleware.checkToken, apiskyController.get);
-router.get('/buscarUsuario/:id', middleware.checkToken, apiskyController.getId);
-router.get('/buscarUsuarioNome/:nome', middleware.checkToken, apiskyController.getNomeUsuario);
-router.get('/excluirUsuario/:id', middleware.checkToken, apiskyController.deleteUsuarios);
 
 
 module.exports = router;
